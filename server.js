8e6ff55c6028695012abc96e2601dfb11f450ae8
cleve gomes <gomescleve@gmail.com>
2017-04-04 11:10:29 +0400

    Update server.js
    added code for html render

--- server.js
+++ server.js
@@ -22,6 +22,26 @@ function setHeaders(res, path) {
 
  
 
+//For HTML RENDER
+// var finalhandler = require('finalhandler')
+// var http = require('http')
+// var serveStatic = require('serve-static')
+ 
+// // Serve up public/ftp folder 
+// var serve = serveStatic(__dirname, {'index': ['index.html', 'index.htm']})
+ 
+// // Create server 
+// var server = http.createServer(function onRequest (req, res) {
+//   serve(req, res, finalhandler(req, res))
+// })
+ 
+// // Listen 
+// server.listen(5556,function(){
+//     console.log('Server running on 5556...');
+// });
+ 
+
+
 
  
  
